6f9abd022cb22c43226e348a2b8dbead6f21caf5
Corvus <wyatta.hancock@gmail.com>
2020-06-10 12:16:24 -0600

    created a basic link between javascript and html

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var countdown = document.getElementById("Timer");
+var HighScore = document.getElementById("Scores")
+var body = document.getElementById("body");
+var button = document.getElementById("begin-test");
