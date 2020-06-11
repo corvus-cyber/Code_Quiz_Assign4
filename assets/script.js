dbd941129b4f2bb74abbafe36e73cb0c0634d8c1
Corvus <wyatta.hancock@gmail.com>
2020-06-11 15:26:57 -0600

    added some formatting for css and created a function for the timer portion of the application

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,19 @@
-var countdown = document.getElementById("Timer");
+var countdownEl = document.querySelector("Timer");
 var HighScore = document.getElementById("Scores")
 var body = document.getElementById("body");
 var button = document.getElementById("begin-test");
+var secondsLeft = 60;
+
+
+function Timer(){
+    var timerInterval = setInterval(function(){
+        secondsLeft --;
+        countdownEl.textContent = secondsLeft + "seconds";
+
+        if (secondsLeft===0){
+            clearInterval(timerInterval);
+            //Create a function for the final score
+        }
+    }, 1000);
+
+}
