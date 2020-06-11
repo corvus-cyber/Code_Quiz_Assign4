e527976e7a3777a9e1c08ed0c632e80ccb07d77d
Corvus <wyatta.hancock@gmail.com>
2020-06-11 16:05:33 -0600

    added pseudocode to javascript

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,39 @@
-var countdownEl = document.querySelector("Timer");
+var countdownEl = document.querySelector("#Timer");
 var HighScore = document.getElementById("Scores")
 var body = document.getElementById("body");
-var button = document.getElementById("begin-test");
+var begintest = document.getElementById("begin-test");
 var secondsLeft = 60;
+var score = 0 
+
+begintest.addEventListener("click", function(){
+    function Timer(){
+        var timerInterval = setInterval(function(){
+            secondsLeft --;
+            countdownEl.textContent = secondsLeft + " seconds";
+
+            if (secondsLeft===0){
+                clearInterval(timerInterval);
+                //Create a function for the final score should secondsLeft === 0 
+            }
+        }, 1000);   
+    }
+    Timer (); 
+    //Issue: timer increases pace when start button is pressed multiple times 
+
+//Create multiple choice questions 
+
+//Make it so that it scores the rounds
+//Should they answer incorrectly, take 5 seconds away from secondsLeft
+//Should they answer correctly, add 5 seconds to SecondsLeft 
+})
+
+//Present final score to user 
+    //
+
+//Loop the timer in so that if the timer === 0 it will take user here 
+
+//Place the option in for the User to put in Initals and score for their score to be recorded 
+
+//Score will be loaded to the local storage 
 
 
-function Timer(){
-    var timerInterval = setInterval(function(){
-        secondsLeft --;
-        countdownEl.textContent = secondsLeft + "seconds";
-
-        if (secondsLeft===0){
-            clearInterval(timerInterval);
-            //Create a function for the final score
-        }
-    }, 1000);
-
-}
