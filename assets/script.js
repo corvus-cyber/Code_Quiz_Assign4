33b3370610ed7c431a1ad0900a8086da5e08785e
Corvus <wyatta.hancock@gmail.com>
2020-06-13 09:18:22 -0600

    added an evaporate class to css so that the start button will disappear when quiz begins. Added four buttons for multiple choice.

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,18 @@
 var countdownEl = document.querySelector("#Timer");
-var HighScore = document.getElementById("Scores")
+var HighScore = document.querySelector("dropdown-menu")
 var body = document.getElementById("body");
 var begintest = document.getElementById("begin-test");
 var secondsLeft = 60;
-var score = 0 
+var score = 0;
+
 
 begintest.addEventListener("click", function(){
+    begintest.classList.add('evaporate')
     function Timer(){
         var timerInterval = setInterval(function(){
             secondsLeft --;
+            //Should they answer incorrectly, take 5 seconds away from secondsLeft
+            //Should they answer correctly, add 5 seconds to SecondsLeft 
             countdownEl.textContent = secondsLeft + " seconds";
 
             if (secondsLeft===0){
@@ -20,15 +24,14 @@ begintest.addEventListener("click", function(){
     Timer (); 
     //Issue: timer increases pace when start button is pressed multiple times 
 
-//Create multiple choice questions 
+    //Create multiple choice questions 
 
-//Make it so that it scores the rounds
-//Should they answer incorrectly, take 5 seconds away from secondsLeft
-//Should they answer correctly, add 5 seconds to SecondsLeft 
+    //Make it so that it scores the rounds
+
 })
 
-//Present final score to user 
-    //
+//Present final score to user within a function
+
 
 //Loop the timer in so that if the timer === 0 it will take user here 
 
